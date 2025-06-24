9bb12212bee4e5530c9da0cd6eb6c9e6b4effa44
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-06-24 15:30:28 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee ={
+    name : 'Garvin',
+    streetAddress: 12390
+};
+// this function does not mutate the original object above(employee)
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+ return {
+    name :'Sam',
+    streetAddress:'11 Broadway'
+ }
+        
+};
+
+// it directly modifies the object
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    employee[key]= value
+return {
+ name : 'Sam',
+ streetAddress:"12 Broadway",
+}
+
+};
+
+function deleteFromEmployeeByKey(employee,key){
+employee= {
+    ...employee
+}
+delete employee.name;
+return employee;
+};
+
+function  destructivelyDeleteFromEmployeeByKey(employee,key) {
+   delete  employee [key] ;
+return employee;
+};
